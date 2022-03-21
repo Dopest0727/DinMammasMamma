6d94f584a38729d6cd7101cf848e3035411edb5a
Dopest0727 <74298827+Dopest0727@users.noreply.github.com>
2022-03-21 11:34:16 +0100

    Done with site

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import Pages from "./pages/Pages";
+import Category from './components/Category';
+import Search from './components/Search';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { GiKnifeFork } from 'react-icons/gi';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav>
+        <GiKnifeFork />
+        <Logo to={'/'}>Din Mammas Mamma</Logo>
+      </Nav>
+      <Search />
+      <Category />
+      <Pages />
+    </div>
+  );
+}
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Lobster Two', cursive;
+  margin-left: 1rem;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg{
+    font-size: 2rem;
+  }
+`;
+
+export default App;
